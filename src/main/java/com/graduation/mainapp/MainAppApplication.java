fbb7185f9f5b40a22edea0b34e11a8354533d7d9
nikolasokolov <35638588+nikolasokolov@users.noreply.github.com>
2019-11-16 12:53:00 +0100

    Initial commit

---
+++ src/main/java/com/graduation/mainapp/MainAppApplication.java
@@ -0,0 +1,15 @@
+package com.graduation.mainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MainAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graduation/mainapp/MainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graduation.mainapp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
