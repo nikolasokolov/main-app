6fb184767e1cb6a50046caab432b524a657a8d93
Nikola Sokolov <nikola.sokolov@endava.com>
2020-01-17 13:37:40 +0100

    Initial implementation of sending pdf document in mail

--- src/main/java/com/graduation/mainapp/service/EmailService.java
+++ src/main/java/com/graduation/mainapp/service/EmailService.java
@@ -1,6 +1,5 @@
 package com.graduation.mainapp.service;
 
 public interface EmailService {
-    void sendMail(String to, String subject, String body);
     void sendMailWithAttachment(String to, String subject, String body, String fileToAttach);
 }
